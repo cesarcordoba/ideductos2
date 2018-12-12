17a60ab31179759efee56f5882b90868f18ac6f2
Your Name <you@example.com>
2018-12-12 12:48:26 -0600

    12 de diciembre 2018 12:48pm

--- http/connection.js
+++ http/connection.js
@@ -14,23 +14,34 @@ var sequelize = new Sequelize('heroku_8336c91bfbfd82e', 'bebb7ad19454a6', '28d75
 });
 */
 
-var sequelize = new Sequelize('ideductos', 'jessem', '#1q2w3e4r', {
-    host: 'jessem.database.windows.net',
-    dialect: 'mssql',
-    port: '1433',
+var sequelize = new Sequelize('ideductos', 'ideductos', '#1q2w3e4r', {
+    host: '35.226.84.114',
+    dialect: 'mysql',
+    port: '3306',
     pool: {
         min: 10,
         max: 300,
         idle: 30000,
-    },
-    dialectOptions: {
-        requestTimeout : 30000,
-        encrypt: true,
-        ssl: {
-            ca: fs.readFileSync('ssl.crt.pem')
-        }
     }
 });
+
+// var sequelize = new Sequelize('ideductos', 'ideductos', '#1q2w3e4r', {
+//     host: '35.226.84.114',
+//     dialect: 'mysql',
+//     port: '3306',
+//     pool: {
+//         min: 10,
+//         max: 300,
+//         idle: 30000,
+//     },
+//     dialectOptions: {
+//         requestTimeout : 30000,
+//         encrypt: true,
+//         ssl: {
+//             ca: fs.readFileSync('ssl.crt.pem')
+//         }
+//     }
+// });
 
 // 
 // sequelize.sync()
